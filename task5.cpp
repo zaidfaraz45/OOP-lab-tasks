354ac3e704e2f69554a053f842d239cc8cbd068f
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-02-04 23:14:50 +0500

    Task done

--- task5.cpp
+++ task5.cpp
@@ -3,5 +3,29 @@ using namespace std;
 
 int main()
 {
+    size_t size;
+    cout << "Enter the size of an array: ";
+    cin >> size;
+
+    int *array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter ineteger " << i + 1 << ": ";
+        cin >> *(array + i);
+    }
+
+    int sum = 0;
+    cout << endl << "Array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << *(array + i) << endl;
+        sum += *(array + i);
+    }
+
+    cout << endl << "Sum of the array is " << sum << "." << endl;
+
+    delete[] (array);
+
     return 0;
 }
