6ea9b798e887f6a5856dc73687ed8a54a1f531ea
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-02-14 02:27:56 +0500

    Task done

--- lab 3/task1.cpp
+++ lab 3/task1.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+using namespace std;
 
+class Wallet
+{
+    string OwnerName;
+    float totalBalance;     //total money in the account
+    float transactionHistory;   //total money spent in transactions
+
+    public:
+        void input()
+        {
+            cout << "Enter the name of the wallet owner: ";
+            getline(cin, OwnerName);
+
+            cout << "Enter the total wallet balance: ";
+            cin >> totalBalance;
+
+            cout << "Enter money spent in transactions: ";
+            cin >> transactionHistory;
+        }
+
+        void addMoney(float money)
+        {
+            totalBalance += money;
+        }
+
+        void spendMoney(float money)
+        {
+            transactionHistory += money;
+        }
+
+        void displayTransactions()
+        {
+            cout << "The total transaction is " << transactionHistory << ".";
+        }
+};
+
+int main()
+{
+    Wallet wallet;
+
+    wallet.input();
+
+    return 0;
+}
