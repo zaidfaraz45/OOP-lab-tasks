9b0248422446e42ff530a97f477f2cf63d8eae26
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-03-05 03:59:03 +0500

    to do

--- lab 5/lab1.cpp
+++ lab 5/lab1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+class Apartment
+{
+    string id;
+    string address;
+    string *ownerName = new string[25];
+
+    public:
+        Apartment(int i, string a, string *on) : id(i), ;
+};
+
+int main()
+{
+
+
+    return 0;
+}
