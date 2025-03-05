726d907bd924b39e5c83c61c597a0bc6f6fdcb4a
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-03-06 04:38:36 +0500

    Task done

--- lab 6/task1.cpp
+++ lab 6/task1.cpp
@@ -1,1 +1,40 @@
 
+#include <iostream>
+using namespace std;
+
+class Employee
+{   
+    public:
+        string name;
+        float salary;
+
+        Employee(string n, float s) : name(n), salary(s) {}
+
+        void display() const
+        {
+            cout << "Name of the employee: " << name << endl;
+            cout << "Salary: $" << salary << endl;
+        } 
+};
+
+class Manager : public Employee
+{
+    float bonus;
+
+    public:
+        Manager (string n , float s, float b) : Employee(n, s), bonus(b) {}
+
+        void display() const
+        {
+            Employee::display();
+            cout << "Bonus: $" << bonus << endl;
+        } 
+};
+
+int main()
+{
+    Manager manager("Zaid Faraz", 89056.2, 10198.4);
+    manager.display();
+
+    return 0;
+}
