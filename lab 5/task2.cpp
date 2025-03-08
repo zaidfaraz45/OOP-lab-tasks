d000865f99c37566723a0a492f9d9dc41e718688
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-03-08 18:17:37 +0500

    Task done

--- lab 5/task2.cpp
+++ lab 5/task2.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
 using namespace std;
 
-class Student
+class University
 {
-    string id;
+    string ID;
     string name;
-    string *examScores = new string[];
+    string *examScores;
+
+    public:
+        University(string id, string n, string *es) : ID(id), name(n) 
+        {
+            examScores = new string[3]; 
+            for (int i = 0; i < 3; i++) 
+            {
+                examScores[i] = es[i];
+            }
+        }
+
+        University(University &obj)
+        {
+            ID = obj.ID;
+            name = obj.name;
+            examScores = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                examScores[i] = obj.examScores[i];
+            }
+        }
+
+        void display()
+        {
+            cout << "\nStudent ID: " << ID << endl;
+            cout << "Student name: " << name << endl;
+            cout << "Exam scores of the student: " << endl ;
+            for (int i = 0; i < 3; i++)
+            {
+                cout << examScores[i] << endl;
+            }
+        }
+
+        ~University() 
+        {
+            delete[] examScores; 
+        }
 };
 
 int main()
 {
+    string scores[3] = {"15", "13", "45"};
 
+    University university("24k-0636", "Zaid Faraz Syed", scores);
+    university.display();
+
+    University university1(university);
+    university1.display();
 
     return 0;
 }
