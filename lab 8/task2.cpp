d6303f5e56a51c69a5ee7c22a82a8d57864bf34e
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-04-11 21:55:22 +0500

    Task done

--- lab 8/task2.cpp
+++ lab 8/task2.cpp
@@ -10,45 +10,54 @@ class Account
         float balance;
 
     public:
-        Account(int aN, float b): accountNumber(aN), balance(b) {}
+        Account(int aN, float b) : accountNumber(aN), balance(b) {}
 
-        friend Manager;
-        friend void transferFunds(Account a1, Account a2, float fund);
-};
+        friend class Manager;
+        friend void transferFunds(Account& a1, Account& a2, float fund);
+    };
 
-void transferFunds(Account a1, Account a2, float fund)
-{
-    if (a1. balance >= fund)
+    void transferFunds(Account& a1, Account& a2, float fund)
     {
-        a1.balance -= fund;
-        a2.balance += fund;
-    }
-
-    else {cout << "Insufficient balance for transfer!" << endl;}
+        if (a1.balance >= fund)
+        {
+            a1.balance -= fund;
+            a2.balance += fund;
+            cout << "Transferred $" << fund << " from Account " << a1.accountNumber << " to Account " << a2.accountNumber << "!" << endl;
+        }
+        else
+        {
+            cout << "Insufficient balance in Account " << a1.accountNumber << " for transfer!" << endl;
+        }
 }
 
 class Manager
 {
     public:
-        Account account;
-
-        void display()
+        void display(Account a)
         {
-            cout << "-------------------------------------------";
+            cout << "-------------------------------------------" << endl;
             cout << "ACCOUNT DETAILS\n";
-            cout << "Account number: " << account.accountNumber << endl;
-            cout << "Balance: $" << account.balance << endl;
+            cout << "Account number: " << a.accountNumber << endl;
+            cout << "Balance: $" << a.balance << endl;
         }
 
-        void deposit(float balance)
+        void deposit(Account& a, float amount)
         {
-            account.balance += balance;
+            a.balance += amount;
+            cout << "Deposited $" << amount << " to Account " << a.accountNumber << "!" << endl;
         }
 
-        void withdraw(float balance)
+        void withdraw(Account& a, float amount)
         {
-            if (balance <= account.balance) {account.balance -= balance;}            
-            else {cout << "Not enough balance!" << endl;}
+            if (amount <= a.balance)
+            {
+                a.balance -= amount;
+                cout << "Withdrew $" << amount << " from Account " << a.accountNumber << "!" << endl;
+            }
+            else
+            {
+                cout << "Not enough balance in Account " << a.accountNumber << "!" << endl;
+            }
         }
 };
 
@@ -57,6 +66,21 @@ int main()
     Account a1(14334, 1000);
     Account a2(13243, 2000);
 
-    Manager m1(14334, 1000);
-    m1.display();
+    Manager m;
+
+    m.display(a1);
+    m.display(a2);
+
+    m.deposit(a1, 500);
+    m.withdraw(a2, 1200);
+
+    m.display(a1);
+    m.display(a2);
+
+    transferFunds(a1, a2, 700);
+
+    m.display(a1);
+    m.display(a2);
+
+    return 0;
 }
