c53b91b74b67e981cc1edb9ba72c41efabef01ed
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-05-04 22:33:58 +0500

    Task done

---
+++ lab 10/task4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string fileName = "vehicles.txt";
+    ifstream file;
+    file.open(fileName, ios::in);
+
+    if (!file.is_open())
+    {
+        cerr << "File could not be opened!\n";
+        return 0;
+    }
+
+    string line;
+    getline(file, line);
+
+    cout << line << endl;
+
+    while(getline(file, line))
+    {
+        istringstream iss(line);
+        string type, id, name, year, extraData, certification;
+
+        getline(iss, type, ',');
+        getline(iss, id, ',');
+        getline(iss, name, ',');
+        getline(iss, year, ',');
+        getline(iss, extraData, ',');
+        getline(iss, certification, ',');
+
+        size_t colonPosition = extraData.find(':');
+        if (colonPosition != string::npos)
+        {
+            string numberStr = extraData.substr(colonPosition + 1); 
+
+            if (type == "AutonomousCar")
+            {
+                float softwareVersion = stof(numberStr);
+                cout << "Software Version for ID " << id << ": " << softwareVersion << endl;
+            }
+            else if (type == "ElectricVehicle")
+            {
+                int batteryCapacity = stoi(numberStr);
+                cout << "Battery Capacity for ID " << id << ": " << batteryCapacity << endl;
+            }
+        }
+        else
+        {
+            cout << "No colon found in ExtraData. Skipping value extraction.\n";
+        }
+    }
+
+    file.close();
+
+    return 0;
+}
