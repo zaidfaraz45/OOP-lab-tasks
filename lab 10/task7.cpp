6e4bc313bafe8a1b539afc20e928c5a5ed212cc2
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-05-06 00:43:04 +0500

    Task done

--- lab 10/task7.cpp
+++ lab 10/task7.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+int main()
+{
+    ofstream file("sensor_log.txt", ios::out);
+
+    if(!file.is_open())
+    {
+        cerr << "Unable to open file!\n";
+        return 0;
+    }
+
+    file << "Sensor 1: 25.5 C\nSensor 2: 98.1 %RH \n";
+    streampos position = file.tellp();
+
+    cout << position << endl;
+
+    return 0;
+}
