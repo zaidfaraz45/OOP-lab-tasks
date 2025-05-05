3ac88e8f3dcbc970eddf0b13627f8d85d761546d
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-05-06 01:06:26 +0500

    Task done

--- lab 10/task8.cpp
+++ lab 10/task8.cpp
@@ -1,2 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+int main()
+{
+    ofstream outFile("config.txt", ios::out);
+
+    if(!outFile.is_open())
+    {
+        cerr << "Unable to open file!\n";
+        return 0;
+    }
+
+    outFile << "AAAAABBBBBCCCCC";
+
+    outFile.close();
+
+    fstream file("config.txt", ios::in | ios::out);
+
+    if (!file.is_open())
+    {
+        cerr << "File could not be opened!\n";
+        return 0;
+    }
+
+    file.seekp(5, ios::beg);
+
+    file << "XXXXX";
+
+    file.close();
+
+    ifstream inFile("config.txt", ios::in);
+
+    if (!inFile.is_open())
+    {
+        cerr << "File could not be opened!\n";
+        return 0;
+    }
+
+    string fileData;
+
+    getline(inFile, fileData);
+
+    cout << fileData;
+
+    inFile.close();
+
+    return 0;
+}
