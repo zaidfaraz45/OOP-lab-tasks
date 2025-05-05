d2816d112b6b77c41a96ef398a9e361f951e59c4
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-05-06 01:20:10 +0500

    Task done

--- lab 10/task9.cpp
+++ lab 10/task9.cpp
@@ -1,2 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+int main()
+{
+    ifstream file;
+    file.open("large_log.txt", ios::in);
+
+    if (!file.is_open())
+    {
+        cerr << "Unable to open file!";
+        return 0;
+    }
+
+    string line;
+    getline(file, line);
+    cout << line;
+
+    streampos position = file.tellg();
+    cout << "\nPosition of pointer: " << position << endl << endl;
+
+    getline(file, line);
+    cout << line;
+
+    position = file.tellg();
+    cout << "\nPosition of pointer: " << position << endl << endl;
+
+    file.close();
+
+    return 0;
+}
