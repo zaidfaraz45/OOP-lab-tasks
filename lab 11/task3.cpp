ebfb2c02e42ee31ebe93543aaff6b0df076c322b
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-05-14 04:47:40 +0500

    Task done

---
+++ lab 11/task3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class InvalidTemperatureException: public exception
+{
+    public:
+        InvalidTemperatureException(double temperature) {}
+};
+
+template <typename T>
+T convertToFahrenheit(T celsius)
+{
+    if (celsius < 273.15)
+    {
+        throw InvalidTemperatureException(celsius);
+    }
+
+    return (celsius * 9/5) + 32;
+}
+
+int main()
+{
+    try 
+    {
+        int celsius = -300;
+        cout << "Attempting to convert " << celsius << "C\n";
+        convertToFahrenheit(-300);
+    }   
+    catch(InvalidTemperatureException &e)
+    {
+        cout << e.what();
+    }
+
+    return 0;
+}
